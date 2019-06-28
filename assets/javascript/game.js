5b9149eeae71335a2e9427a488695e4f3384fcf1
Mac Balisage <kidnamemac@gmail.com>
2019-06-28 03:34:10 -0400

    Finally got the game to work properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,60 @@
 var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var wins = 0
-var losses = 0
-var attempts = 9
+var wins = 0;
+var losses = 0;
+var numAttempts = 9;
+var guessChoices = [];
 
-var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var computerChoiceText = document.getElementById('para');
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
+//var directionsText = document.getElementById("directions-text");
+//var userChoiceText = document.getElementById("userchoice-text");
+//var computerChoiceText = document.getElementById('para');
+//var winsText = document.getElementById("wins-text");
+//var lossesText = document.getElementById("losses-text");
 
 document.onkeyup = function (event) {
 
     var userGuess = event.key;
+    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
-    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)]
+    //console.log(computerGuess);
 
-     var userGuess = event.key.toLowerCase();
-     
-     // console.log(computerGuess)
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+    //var userGuess = event.key.toLowerCase();
+
+    if (options.indexOf(userGuess) > -1) {
         if (userGuess === computerGuess) {
-        wins++;
-        attempts = 9
-       
+            wins++;
+            numAttempts = 9;
+            guessChoices = [];
         }
-        else if (userGuess !== computerGuess) {
-        attempts = -1
-        
+        if (userGuess !== computerGuess) {
+            numAttempts--;
+            guessChoices.push(userGuess);
         }
-        else {
-        loses++    
-        };
+        if (numAttempts === 0) {
+            numAttempts = 9;
+            losses++;
+            guessChoices = [];
+        }
 
-    
+        var html =
+            "<h1> The  Psychic Game </h1>" +
+            "<p> Guess what letter I'm thinking of </p>" +
+            "<p>Wins: " + wins + "</p>" +
+            "<p>Loses: " + losses + "</p>" +
+            "<p>Attemps left: " + numAttempts + "</p>" +
+            "<p>Your guesses so far: " + guessChoices.join(", ") + "</p>";
 
-    directionsText.textContent = "";
+        document.querySelector("#game").innerHTML = html
+        //directionsText.textContent = "";
 
-    userChoiceText.textContent = "You chose: " + userGuess;
-    computerChoiceText.textContent = "The computer chose: " + computerGuess;
-    winsText.textContent = "wins: " + wins;
-    lossesText.textContent = "losses: " + losses;
-    attempsText.textContent = "attempts: ";
-
+        //userChoiceText.textContent = "You chose: " + userGuess;
+        //computerChoiceText.textContent = "The computer chose: " + computerGuess;
+        //winsText.textContent = "wins: " + wins;
+        //lossesText.textContent = "losses: " + losses;
+        //attempsText.textContent = "attempts: ";
+    }
     //computerChoiceText.textContent = computerGuess;
-
     //console.log(computerChoiceText.textContent);
-
-}
+};
